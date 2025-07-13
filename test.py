f66c4f5dae3cf7e5a2825c77ef3cbe1154c662eb
John Fiore <142550056+john-fiore@users.noreply.github.com>
2025-07-13 17:38:12 -0500

    Initial Commit

---
+++ test.py
@@ -0,0 +1,12 @@
+# Source Code viewers,
+#
+# Ignore this file. This was just to test out
+# the rounding function to see if it worked
+# correctly as intended. For reference, I'm
+# unfamiliar with the round() method.
+#
+# - John Fiore, the dev
+
+number = 6
+
+print(round(number / 100, 2))
